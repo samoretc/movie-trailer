70929cb82d3aa3f4f91a458acee083e92ca4f9ef
Ted's MacBook <tedsmacbook@Tedss-MacBook-Air.local>
2015-09-21 17:31:11 -0500

    Initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import fresh_tomatoes
+import media 
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+titanic = media.Movie("Titanic", "A romance about the titanic", "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",  "https://www.youtube.com/watch?v=zCy5WQ9S4c0")
+movies = [toy_story, titanic]
+fresh_tomatoes.open_movies_page(movies)
